ad2bf4643d328be921f59bf6bf801bbf532769b4
Jewell Day <day348@gmail.com>
2020-03-12 15:10:24 -0700

    tried to get more stats to figure out how to paralize everything

--- neural_network/test2.py
+++ neural_network/test2.py
@@ -1,9 +1,11 @@
 import numpy as np
+import time
+
 class NeuralNetwork:
     def __init__(self):
         np.random.seed(10) # for generating the same results
-        self.wij   = np.random.rand(3,4) # input to hidden layer weights
-        self.wjk   = np.random.rand(4,1) # hidden layer to output weights
+        self.wij   = np.random.rand(3,50) # input to hidden layer weights
+        self.wjk   = np.random.rand(50,1) # hidden layer to output weights
         
     def sigmoid(self, x, w):
         z = np.dot(x, w)
@@ -13,17 +15,25 @@ class NeuralNetwork:
         return self.sigmoid(x, w) * (1 - self.sigmoid(x, w))
     
     def gradient_descent(self, x, y, iterations):
+        calcTime =0
+        gradientTime = 0
         for i in range(iterations):
+            start_time = time.time()
             Xi = x
             Xj = self.sigmoid(Xi, self.wij)
             yhat = self.sigmoid(Xj, self.wjk)
+            calcTime = calcTime + time.time() -start_time
+            start_time = time.time()
             # gradients for hidden to output weights
             g_wjk = np.dot(Xj.T, (y - yhat) * self.sigmoid_derivative(Xj, self.wjk))
             # gradients for input to hidden weights
             g_wij = np.dot(Xi.T, np.dot((y - yhat) * self.sigmoid_derivative(Xj, self.wjk), self.wjk.T) * self.sigmoid_derivative(Xi, self.wij))
             # update weights
+            gradientTime = gradientTime + time.time() -start_time
             self.wij += g_wij
             self.wjk += g_wjk
+        print("calc time: ", calcTime)
+        print("gradient time: ", gradientTime)
         print('The final prediction from neural network are: ')
         print(yhat)
 if __name__ == '__main__':
