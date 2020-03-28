5a32071074fe3824a8ba9319b77b476a286eb1d5
Jewell Day <day348@gmail.com>
2020-03-27 20:38:20 -0700

    parralized the gradient descent

--- neural_network/nn_class_test.py
+++ neural_network/nn_class_test.py
@@ -1,12 +1,13 @@
 import NeuralNet as nn  
 import numpy as np
 
-x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-y = np.array([[0, 1, 1, 0]])
+x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]]*50)
+y = np.array([[0, 1, 1, 0]*50])
 
 
-net = nn.NeuralNet([7,7],[3,50,1])
-net.backProp(x,y[0],1,1000)
+net = nn.NeuralNet([7,7,7],[3,1000,100,1])
+if __name__ == "__main__":
+    net.backProp(x,y[0],1,1)
 
 
 
