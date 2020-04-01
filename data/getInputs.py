7450f9303094f929006a8f6c74eadc4f38875306
BrodyPearman <43056716+BrodyPearman@users.noreply.github.com>
2020-04-01 15:50:05 -0700

    Add files via upload

---
+++ data/getInputs.py
@@ -0,0 +1,33 @@
+def getInputs(tic, date, globe, datesToNums):
+    listOfDates= list(vals.index)
+    listOfNums=[x for x in range(0,2889)]
+    datesToNums= dict(zip(listOfDates, listOfNums))
+    new_dict = dict([(value, key) for key, value in datesToNums.items()]) 
+    beginningIndex= datesToNums[date]
+    endIndex=beginningIndex+20
+    relevantDates=[]
+    for i in range(beginningIndex, endIndex):
+        relevantDates.append(new_dict[i])
+    output=[]
+    for i in relevantDates:
+        output.append(globe['low'][i])
+        output.append(globe['high'][i])
+        output.append(globe['average'][i])
+        output.append(globe['volume'][i])
+        output.append(globe['close'][i])
+        output.append(globe['open'][i])
+        output.append(globe['range'][i])
+        output.append(globe['twelveDay'][i])
+        output.append(globe['twentySixDay'][i])
+        output.append(globe['volumeEMA'][i])
+        output.append(globe['singleDay'][i])
+        output.append(globe['dayToDay'][i])
+        output.append(globe['fiftyTwoDayHigh'][i])
+        output.append(globe['fiftyTwoWeekHigh'][i])
+        output.append(globe['fiftyTwoDayLow'][i])
+        output.append(globe['fiftyTwoWeekLow'][i])
+        output.append(globe['fiftyTwoWeekAverage'][i])
+        output.append(globe['fiftyTwoDayStandDev'][i])
+        output.append(globe['fiftyTwoWeekStandDev'][i])
+    return np.array(output)
+    
