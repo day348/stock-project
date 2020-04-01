4f594b4f0e639b412e87159e31255f36ec33b52b
Jewell Day <day348@gmail.com>
2020-04-01 16:23:26 -0700

    rearrangin

--- nn_class_test.py
+++ nn_class_test.py
@@ -1,11 +1,11 @@
 import neural_network.NeuralNet as nn  
 import numpy as np
 
-x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-y = np.array([0, 1, 1, 0])
+x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]]*100)
+y = np.array([0, 1, 1, 0]*100)
 
 
-net = nn.NeuralNet([7,7],[3,10,1])
+net = nn.NeuralNet([7,7],[3,100,1])
 if __name__ == "__main__":
     net.backProp(x,y,1,10)
 
