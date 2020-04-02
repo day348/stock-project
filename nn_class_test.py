fd95f67175f4471d33baa6870f15be2b3a6c8f73
Jewell Day <day348@gmail.com>
2020-04-01 19:10:30 -0700

    Finished NeuralNet Optimization

--- nn_class_test.py
+++ nn_class_test.py
@@ -1,13 +1,14 @@
 import neural_network.NeuralNet as nn  
 import numpy as np
 
-x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]]*100)
-y = np.array([0, 1, 1, 0]*100)
+x = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+y = np.array([0, 1, 1, 0])
 
 
-net = nn.NeuralNet([7,7],[3,10,1])
+net = nn.NeuralNet([7,7],[3,100,1])
 if __name__ == "__main__":
-    net.backProp(x,y,1,1)
-
-
+    error_change = net.backProp(x,y,learnRate = 1,iterations = 100)
+    for i in range(4):
+        print(net.calculateOutput(x[i])[-1][-1])
+    print(error_change)
 
